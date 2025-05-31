9419cabade956aad0a7d22bb83be3cb0fe2552e1
Jorge Axel Hernandez <20001544@alumnos.utleon.edu.mx>
2025-05-30 21:19:47 -0600

    first commit

---
+++ app/src/main/java/com/example/workadministration/HomeActivity.java
@@ -0,0 +1,39 @@
+package com.example.workadministration;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity  {
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    startActivity(new Intent(this, HomeActivity.class));
+                    return true;
+                case R.id.nav_products:
+                    startActivity(new Intent(this, ProductsActivity.class));
+                    return true;
+                case R.id.nav_customers:
+                    startActivity(new Intent(this, CustomersActivity.class));
+                    return true;
+                case R.id.nav_tickets:
+                    startActivity(new Intent(this, TicketsActivity.class));
+                    return true;
+            }
+            return false;
+        });
+    }
+}
